fa9da2895ef17f9fa820fca3cb5b4b09337cad4b
Roni Frumkis <roni.frumkis@post.runi.ac.il>
2025-11-22 11:03:03 +0200

    finish 2

--- Primes.java
+++ Primes.java
@@ -1,5 +1,27 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        System.out.println( "Prime numbers up to 30:");
+        int N = 30;
+        int count = 0;
+
+        for (int i = 2; i <= N ; i++) {
+            if (i == 2 || i == 3 || i == 5) {
+                System.out.println(i); 
+                count ++;
+                }
+            else if (i % 2 != 0) {
+                    if (i % 3 != 0) {        
+                        if (i % 5 != 0) {
+                        System.out.println(i); 
+                        count ++;
+                    }
+                }
+            }
+        }
+            double totalNumbers = N;
+            double percentage = ((double) count / totalNumbers) * 100;
+            System.out.println("There are " + count + " primes between " + 2 + " and " + N + " ("  + (int) percentage + "%" + " are primes)");
+        
     }
-}
+ }
+   
